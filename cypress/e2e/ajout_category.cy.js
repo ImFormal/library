d0c302d870aa7caf9576d1cbeda9cf2ca8157468
ImFormal <thubert.maxime.pro@gmail.com>
2025-03-27 14:03:04 +0100

    test cypress

---
+++ cypress/e2e/ajout_category.cy.js
@@ -0,0 +1,26 @@
+describe('Tests ajout catégorie', () => {
+  //Arrange
+  beforeEach(() => {
+      cy.visit('https://127.0.0.1:8000/category/new'); 
+  });
+
+  it('ajoute une catégorie valide', () => {
+    //Act
+      cy.get('#category_label').clear(); 
+      cy.get('#category_label').type('Jardinage'); 
+      cy.get('.btn').click();
+
+    //Assert
+      cy.get('td').should('be.visible').contains('Jardinage');
+  });
+
+
+  it('ajoute une catégorie non-valide', () => {
+    //Act
+      cy.get('#category_label').clear(); 
+      cy.get('.btn').click();
+
+    //Assert
+      cy.url().should('eq', 'https://127.0.0.1:8000/category/new') // Le form n'a pas pu être validé donc on est toujours sur la même page
+  });  
+});
